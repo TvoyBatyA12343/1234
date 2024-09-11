01162b55961ee7537f34bd17c55742eaab4b9a5a
TvoyBatyA12343 <149304533+TvoyBatyA12343@users.noreply.github.com>
2024-09-11 22:44:01 +0300

    Add files via upload

---
+++ demehin.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>;
+
+int main() {
+	int matrix[3][4] = {
+		{1, 2, 3, 4},
+		{23, 24, 25, 26},
+		{5, 6, 7, 8}
+	};
+	for (int i = 0; i < 3; i += 1) {
+		for (int j = 0; j < 4; j += 1) {
+			if (j == 3) {
+				std::cout << matrix[i][j]; // без пробела после последнего числа в строке
+			}
+			else {
+				std::cout << matrix[i][j] << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "------------------ \n"; // разделитель пунктов задания
+
+	for (int i = 0; i < 4; i += 1) {
+		for (int j = 0; j < 3; j += 1) {
+			if (j == 2) {
+				std::cout << matrix[j][i]; // без пробела после последнего числа в строке
+			}
+			else {
+				std::cout << matrix[j][i] << " ";
+			}
+	
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
